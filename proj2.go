800a3148f558a86a49ff00f13377c66d28a475b3
William Smith <williampsmith@berkeley.edu>
2017-10-12 19:27:29 -0700

    change byte encoding for print statements

--- proj2.go
+++ proj2.go
@@ -228,7 +228,7 @@ func InitUser(username string, password string) (userdataptr *User, err error) {
 	}
 
 	ciphertext := CFBEncrypt(encryptKey, userJSON)
-	fmt.Printf("Stored MAC: %s \n", HMAC(macKey, ciphertext))
+	fmt.Printf("Stored MAC: %s \n", hex.EncodeToString(HMAC(macKey, ciphertext)))
 	emac := EMAC{
 		ciphertext: ciphertext,
 		mac:        HMAC(macKey, ciphertext),
@@ -273,7 +273,7 @@ func GetUser(username string, password string) (userdataptr *User, err error) {
 	mac := HMAC(macKey, emac.ciphertext)
 	if !userlib.Equal(mac, emac.mac) {
 		// TODO: Remove. FOR DEBUGGING
-		fmt.Printf("Computed MAC: %s, Stored MAC: %s \n", macKey, emac.mac)
+		fmt.Printf("Computed MAC: %s, Stored MAC: %s \n", hex.EncodeToString(macKey), hex.EncodeToString(emac.mac))
 		return nil, errors.New("Error. Data has been tampered with.")
 	}
 
